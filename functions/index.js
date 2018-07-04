f6649f4db6071948e6275b567e39d3221dea5e15
programmer5000-com <programmer5000@programmer5000.com>
2018-07-04 06:52:52 -0400

    Fix a lot of the functions

--- functions/index.js
+++ functions/index.js
@@ -19,7 +19,10 @@ admin.initializeApp();
 const db = admin.firestore();
 //const bucket = admin.storage().bucket();
 
-const imagesRef = db.collection("images");
+const basePath = process.env.BASEPATH || process.argv[2] || (process.env.NODE_ENV === "production" ? "images" : "") ||  "test_images";
+console.log(basePath);
+
+const imagesRef = db.collection(basePath);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -27,10 +30,11 @@ const imagesRef = db.collection("images");
 
 
 const shortCache = (req, res, next) => {
-console.log("hi");
   res.set("Cache-Control", "private, max-age=1");
   next();
 };
+
+app.use(shortCache);
 
 const authenticate = (req, res, next) => {
   if(req.query.token){
@@ -50,8 +54,6 @@ const authenticate = (req, res, next) => {
   });
 };
 
-app.use(shortCache);
-
 app.use(base + "authcheck", authenticate);
 app.get(base + "authcheck", (req, res) => res.send("Auth successful."));
 
@@ -59,20 +61,26 @@ app.get(base, (req, res) => {
   res.send("Hello, World!");
 });
 
-app.get(base + "picts/new", (req, res) => {
+app.get(base + "picts/get", (req, res) => {
   let start = 0;
   let end = 20;
   if(req.query.start || req.query.end){
     start = Math.max(Math.min(parseInt(req.query.start), 1000000), 0);
-    end = Math.min(Math.max(Math.min(parseInt(req.query.end), 1000000), 0), start);
+    end = Math.max(Math.min(parseInt(req.query.end), 1000000), start);
     if(typeof start !== "number" || typeof end !== "number" || Number.isNaN(start) || Number.isNaN(start)){
       return res.status(400).json({"error": "Invalid or missing start / end query parameter"});
     }
   }
-  const prom = imagesRef.orderBy("timeStamp", "desc").orderBy("usersVoted", "desc").startAt(start).endAt(end);
-  console.log(prom);
+	console.log("start", start, req.query.start, "end", end, req.query.end);
+  const prom = imagesRef.
+	orderBy("timeStamp", "desc").
+	orderBy("numUsersVoted", "desc").
+	startAt(start).
+	limit(end - start + 1).
+	get();
   prom.then(snapshot => {
     let data = {};
+		console.log(snapshot.docs);
     snapshot.forEach(doc => {
       data[doc.id] = doc.data();
     });

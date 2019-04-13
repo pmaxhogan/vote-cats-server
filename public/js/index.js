59288ef08104e4dfa79eebcf1befa3c033f63711
programmer5000-com <programmer5000@programmer5000.com>
2019-04-13 12:38:12 -0400

    Fix buttons

--- public/js/index.js
+++ public/js/index.js
@@ -59,18 +59,16 @@ const addImgs = num => {
 	    panel.innerHTML = `
 <!-- <h2>A cute cat</h2> -->
 <img class = "mdc-card__media" src = "${pict.url}"/>
-<p class="mdc-card__supporting-text">
-	<i class="heart mdc-icon-toggle material-icons" role="button" aria-pressed="false"
-	   aria-label="Add to favorites" tabindex="0"
-	   data-toggle-on='{"label": "Remove from favorites", "content": "favorite"}'
-	   data-toggle-off='{"label": "Add to favorites", "content": "favorite_border"}'>
-	  favorite_border
-	</i>
-	<i class="material-icons mdc-icon-toggle delete" role="button" aria-pressed="false"
-	   aria-label="Delete this picture" tabindex="0">
-	  delete
-	</i>
-</p>`;
+<div class="mdc-card__actions">
+  <button class="mdc-icon-button mdc-card__action mdc-card__action--icon"
+     aria-pressed="false"
+     aria-label="Add to favorites"
+     title="Add to favorites">
+   <i class="material-icons mdc-icon-button__icon mdc-icon-button__icon--on">favorite</i>
+   <i class="material-icons mdc-icon-button__icon">favorite_border</i>
+  </button>
+  <button class="material-icons mdc-icon-button mdc-card__action mdc-card__action--icon" title="Delete">delete</button>
+</div>`;
 	    getShortestColumn().appendChild(panel);
 	    mdc.iconButton.MDCIconButtonToggle.attachTo(panel.querySelector("i"));
 			panel.querySelector(".delete").addEventListener("click", () => {
